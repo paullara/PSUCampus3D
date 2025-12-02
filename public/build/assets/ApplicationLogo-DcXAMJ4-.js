8b4c6399e6bc2c9450ad46114e5768ee3d848aad
Jan Philip Salvador <salvadorjanphilip@gmail.com>
2025-12-02 16:35:52 +0000

    Ignore public folders for uploads and build

--- public/build/assets/ApplicationLogo-DcXAMJ4-.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./app-C6JDbxEX.js";function e(o){return s.jsx("div",{className:"flex justify-center items-center",children:s.jsx("img",{src:"/logo/psu.png",alt:"PSU Logo",className:"h-20 w-20 sm:h-28 sm:w-28 md:h-32 md:w-32 object-contain drop-shadow-lg",...o})})}export{e as A};
